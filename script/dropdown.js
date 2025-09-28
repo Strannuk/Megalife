878a9a7a21f3d328ca12e093fa1bf4597edb4a6f
Kirill Belokamenskyi <belokamensky.kirill@yandex.ru>
2025-09-29 02:35:53 +0300

    Правка выпадающего списка на телефоне2

--- script/dropdown.js
+++ script/dropdown.js
@@ -14,7 +14,7 @@ function toggleDropdown(button) {
     li.classList.add('open'); 
     dropdown.classList.add('show-dropdown');
     const scrollHeight = dropdown.scrollHeight;
-    dropdown.style.height = scrollHeight + 40 + 'px';
+    dropdown.style.height = scrollHeight + 20 + 'px';
   } else {
     li.classList.remove('open');
     dropdown.style.height = '0';
