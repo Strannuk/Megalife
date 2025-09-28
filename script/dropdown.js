9ac16344158761aabab8c082cb112738c36e4505
Kirill Belokamenskyi <belokamensky.kirill@yandex.ru>
2025-09-29 00:59:26 +0300

    редактирование выпадающего спика

--- script/dropdown.js
+++ script/dropdown.js
@@ -1,5 +1,23 @@
 function toggleDropdown(button) {
-  const dropdown = button.closest('li').querySelector('.dropdown');
-  dropdown.classList.toggle('show-dropdown');
-}
+  const li = button.closest('li');
+  const dropdown = li.querySelector('.dropdown');
 
+  document.querySelectorAll('.dropdown').forEach(d => {
+    if (d !== dropdown && d.classList.contains('show-dropdown')) {
+      d.style.height = '0';
+      d.classList.remove('show-dropdown');
+      d.closest('li').classList.remove('open'); 
+    }
+  });
+
+  if (!dropdown.classList.contains('show-dropdown')) {
+    li.classList.add('open'); 
+    dropdown.classList.add('show-dropdown');
+    const scrollHeight = dropdown.scrollHeight;
+    dropdown.style.height = scrollHeight + 50 + 'px';
+  } else {
+    li.classList.remove('open');
+    dropdown.style.height = '0';
+    dropdown.classList.remove('show-dropdown');
+  }
+}
